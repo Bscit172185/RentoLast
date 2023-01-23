6546fc755149bbb3cff53402b7191bc9c62cf038
nihar <bscit172185@gmail.com>
2023-01-23 17:40:50 +0530

    ForgotPassword_Activity_update-V1.0.0.2

---
+++ app/src/main/java/com/example/rento/Select_Add_Product_Activity.java
@@ -0,0 +1,14 @@
+package com.example.rento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Select_Add_Product_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_add_product);
+    }
+}
