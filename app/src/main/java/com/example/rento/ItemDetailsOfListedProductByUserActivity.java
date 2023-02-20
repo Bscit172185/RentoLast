ba01a09cb46434a76bc8212db1061741f2811ddd
7HIN <bscit172185@gmail.com>
2023-02-20 19:44:41 +0530

    new activity layout of listed products request acceptor cardview

---
+++ app/src/main/java/com/example/rento/ItemDetailsOfListedProductByUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.rento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemDetailsOfListedProductByUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details_of_listed_product_by_user);
+    }
+}
