f94c3cd5f2206648387df0752eef905f514cf867
7HIN <bscit172185@gmail.com>
2023-02-22 17:54:09 +0530

    new activity layout of listed products request acceptor cardview

---
+++ app/src/main/java/com/example/rento/ProcessForPaymentActivity.java
@@ -0,0 +1,14 @@
+package com.example.rento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProcessForPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_for_payment);
+    }
+}
